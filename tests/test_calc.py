50a25c3909a998177cc1e977b4c1e2bb3ab361fb
Takeda-Takumi <TakedaTakumi6108@gmail.com>
2022-10-16 19:59:52 +0900

    coverage

--- tests/test_calc.py
+++ tests/test_calc.py
@@ -5,24 +5,24 @@ def test_add_01():
     assert Calc(9, 2).add() == 11
 
 
-# def test_add_02():
-#     assert Calc(-9, 2).add() == -7
+def test_add_02():
+    assert Calc(-9, 2).add() == -7
 
 
-# def test_dif_01():
-#     assert Calc(9, 2).dif() == 7
+def test_dif_01():
+    assert Calc(9, 2).dif() == 7
 
 
-# def test_dif_02():
-#     assert Calc(-9, 2).dif() == -11
+def test_dif_02():
+    assert Calc(-9, 2).dif() == -11
 
 
-# def test_seki_01():
-#     assert Calc(9, 2).seki() == 18
+def test_seki_01():
+    assert Calc(9, 2).seki() == 18
 
 
-# def test_seki_02():
-#     assert Calc(-9, 2).seki() == -18
+def test_seki_02():
+    assert Calc(-9, 2).seki() == -18
 
 
 # def test_shou_01():
