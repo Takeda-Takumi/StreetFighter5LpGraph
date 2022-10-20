4e2207653b381375a5c57348c787e2ca4218d78b
Takeda-Takumi <TakedaTakumi6108@gmail.com>
2022-10-20 22:32:00 +0900

    [add] LPグラフの表示

--- main/display_lp_graph.py
+++ main/display_lp_graph.py
@@ -1,4 +1,6 @@
+import matplotlib.pyplot as plt
 import MySQLdb
+import matplotlib.dates as mdate
 
 
 def read_mysql_enviroment():
@@ -22,9 +24,23 @@ connection = MySQLdb.connect(
 
 cursor = connection.cursor()
 
-cursor.execute("select access_date, league_point from online_match where fighters_id = 'test' order by access_date;")
+cursor.execute("""
+    select access_date, league_point
+    from online_match
+    where fighters_id = 'test'
+    order by access_date
+;""")
 
-for (access_date, league_point) in cursor:
-    print(access_date, league_point)
+#タプルを転置してリストにする
+data = [list(tup) for tup in zip(*cursor.fetchall())]
+
+fig, ax = plt.subplots()
+fig.autofmt_xdate()
+ax.plot(data[0], data[1])
+
+formatter = mdate.DateFormatter("%Y-%m-%d")
+ax.xaxis.set_major_formatter(formatter)
+
+fig.savefig('lpgraph/out.png')
 
 connection.close()
